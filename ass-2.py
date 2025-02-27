b34d135e9460d662ef66b12f6830393b9685fbe2
sushma-regidi <regidisushma@gmail.com>
2025-02-27 21:32:01 +0530

    v1

---
+++ ass-2.py
@@ -0,0 +1,49 @@
+# PYTHON NUMBERS
+# NUMERIC-INT,FLOAT,COMPLEX NUMBERS
+a=5
+print(a)
+print(type(a))
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+# FLOAT
+print(type(a/b))
+c=6+5j
+d=7+1j
+print(type(c))
+print(c+d)
+print(c-d)
+#  (a + bi) * (c + di) = (ac – bd) + (ad + bc)i.
+print(c*d)
+print(c/d)
+# DICTIONARY
+dict1={1:'sushma',2:'roshini',3:'kumari'}
+print(type(dict1))
+print(dict1[2])
+# BOOLEAN
+e=3
+print(e==3)
+print(e==5)
+print(int(True))
+print(int(False))
+# SEQUENCE-STRING,LIST,TUPLE
+str="sushma"
+print(type(str))
+print(str[3])
+list=[1,2,3,4,5,[0,8]]
+print(list)
+print(type(list))
+print(list[5])
+# LIST IS mutable
+list[5]=9
+print(list)
+tup=(1,"rrr",[6,4])
+print(type(tup))
+# TUPLE IS IMMUTABLE
+# tup[0]=10
+# print(tup)
+set={'a','s','d','f','g'}
+print(type(set))
+    
---
+++ index.py
@@ -0,0 +1,40 @@
+# a=3+5j
+# b=6+4j
+# print(type(a))
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a**b)
+# print(a//b)
+# for i in range(0,21,3):
+#     print(i)
+# for i in range(0,21):
+#     if i % 3 == 0:
+#         print(i)
+# for i in range(1,19,2):
+#     print(i)
+
+# NESTED loops 
+# for j in range(1,11):
+    # if j != 5 and j != 7:
+        # print(j)
+    #  for i in range(1,31):
+    #     print(j,i)
+# num1 = 3
+# while num1 < 5:
+#     print("kill.....")
+#     print("still fighting")
+#     num1+=1
+# i = 5
+# while i < 26:
+#     if i % 2 == 0:
+#         print( i,"even")
+#     else :
+#         print("odd")
+# i+=1
+j = 1
+while j < 11 :
+      for i in range(1,31):
+         print(j)
+ j+=1  
